61f53c88308baff07d9429885407f110b14a041b
hiroki <i.hlrrr0@gmail.com>
2025-03-05 12:26:31 +0900

    確認画面でgtmが誤っていたので、修正

--- lp03/confirm.php
+++ lp03/confirm.php
@@ -17,20 +17,20 @@
 <meta charset="utf-8">
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K3FJ62F');</script>
-<!-- End Google Tag Manager -->
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KBXMTGHC');</script>
+	<!-- End Google Tag Manager -->
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <title>入力内容確認</title>
 <link rel="stylesheet" href="css/form.css">
 </head>
 <body id="confirm">
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3FJ62F"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBXMTGHC"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
 <div class="wrap">
 	<div class="contents">
 		<section class="confirm">
